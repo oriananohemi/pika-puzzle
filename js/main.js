f2b6819fb570bba831563e6e5f740cf97689b999
Oriana Reina <oriana.nohemi22@gmail.com>
2019-08-13 21:50:09 -0500

    Se declararon las variables relacionadas con el movimiento de las piezas y se definio la funcion seleccionarElemento

--- js/main.js
+++ js/main.js
@@ -8,4 +8,11 @@ for (var i = 0; i < piezas.length; i++) {
   piezas[i].setAttribute("height", tamHeight[i]);
   piezas[i].setAttribute("x", Math.floor(Math.random() * 10 + 1));
   piezas[i].setAttribute("y", Math.floor(Math.random() * 409 + 1));
+  piezas[i].setAttribute("onmousedown", "seleccionarElemento(evt)");
 }
+
+var elementSelect = 0;
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
